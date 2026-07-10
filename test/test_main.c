23b5c882707dd9056fefd39fdf50aa53d3cdcb41
TheLeBerton <theleberton@gmail.com>
2026-07-10 03:06:07 +0200

    REMOVE old test.c

---
+++ test/test_main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#include "test_utils.h"
+
+void test_lexer_suite(void);
+void test_read_file_suite(void);
+
+int main(void)
+{
+	test_read_file_suite();
+	test_lexer_suite();
+
+	printf("%d/%d tests passed\n", g_test_run - g_tests_failed, g_test_run);
+	return (g_tests_failed != 0);
+}
